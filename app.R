5160bd7855bedc2e79113d62dde6187e0a3e730a
Shea0121 <sgelsomino21@gmail.com>
2025-08-04 15:36:36 -0400

    Add files via upload
    shiny app code

---
+++ app.R
@@ -0,0 +1,169 @@
+library(shiny)
+library(dplyr)
+library(rebird)
+library(sf)
+library(leaflet)
+library(leaflet.extras)
+library(gt)
+library(lubridate)
+
+priority_species <- c(
+  "American Black Duck", "American Oystercatcher", "Bald Eagle", 
+  "Black-crowned Night-Heron", "Cerulean Warbler", "Saltmarsh Sparrow", 
+  "Upland Sandpiper", "Wood Thrush", "Piping Plover", "Least Tern", 
+  "Northern Harrier", "Bobolink", "Prairie Warbler", "Eastern Meadowlark", 
+  "Sedge Wren", "Great Egret", "Northern Bobwhite", "American Woodcock", 
+  "Marbled Godwit", "Swainson's Hawk", "Eastern Whip-poor-will", 
+  "Ruby-throated Hummingbird", "Black-billed Cuckoo", "Scarlet Tanager", 
+  "White-eyed Vireo", "Common Nighthawk", "Little Blue Heron", 
+  "Barn Owl", "Grasshopper Sparrow"
+)
+
+# Load NDDB shapefile once (make sure the path is correct)
+nddb <- sf::st_read("Natural_Diversity_Database/Natural_Diversity_Database.shp", quiet = TRUE) %>%
+  st_transform(crs = 4326)
+
+ui <- fluidPage(
+  titlePanel("Priority Bird Sightings - Connecticut (Last 30 Days)"),
+  
+  tabsetPanel(
+    tabPanel("Sightings Frequency", 
+             gt_output("freq_table")
+    ),
+    tabPanel("Sightings & Conservation Map",
+             leafletOutput("sightings_map", height = 600)
+    ),
+    tabPanel("Heatmap of Sightings",
+             leafletOutput("heatmap_map", height = 600)
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  # Get recent sightings - reactive to allow refresh
+  priority_recent <- reactive({
+    recent <- ebirdregion(loc = "US-CT", back = 30, max = 5000)
+    recent %>%
+      filter(comName %in% priority_species) %>%
+      mutate(obsDt = as.Date(obsDt))
+  })
+  
+  # Summary table data
+  priority_summary <- reactive({
+    priority_recent() %>%
+      count(comName, sort = TRUE) %>%
+      rename(
+        "Bird Name" = comName,
+        "Sightings in Last 30 Days" = n
+      )
+  })
+  
+  # Render frequency table with gt
+  output$freq_table <- render_gt({
+    priority_summary() %>%
+      gt() %>%
+      tab_header(title = "Bird Sightings in the Last 30 Days") %>%
+      cols_label(
+        `Bird Name` = "Bird Species",
+        `Sightings in Last 30 Days` = "Number of Sightings"
+      ) %>%
+      fmt_number(
+        columns = `Sightings in Last 30 Days`,
+        decimals = 0
+      ) %>%
+      tab_options(
+        table.font.size = "medium",
+        row.striping.background_color = "#f9f9f9",
+        heading.title.font.size = 16,
+        heading.title.font.weight = "bold"
+      )
+  })
+  
+  # Prepare sf points for mapping
+  priority_sf <- reactive({
+    df <- priority_recent()
+    st_as_sf(df, coords = c("lng", "lat"), crs = 4326) %>%
+      mutate(
+        longitude = st_coordinates(.)[,1],
+        latitude = st_coordinates(.)[,2]
+      )
+  })
+  
+  # Color palette for sightings by date
+  pal <- reactive({
+    colorNumeric(palette = "YlOrRd", domain = as.numeric(priority_recent()$obsDt))
+  })
+  
+  # Sightings & conservation map
+  output$sightings_map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      addPolygons(data = nddb,
+                  fillColor = "green",
+                  fillOpacity = 0.3,
+                  weight = 1,
+                  color = "darkgreen",
+                  group = "NDDB Areas") %>%
+      addCircleMarkers(data = priority_sf(),
+                       radius = 6,
+                       fillColor = ~pal()(as.numeric(obsDt)),
+                       fillOpacity = 0.8,
+                       stroke = FALSE,
+                       popup = ~paste0("<b>", comName, "</b><br>", obsDt, "<br>Location: ", locName),
+                       group = "Bird Sightings") %>%
+      addLayersControl(
+        overlayGroups = c("NDDB Areas", "Bird Sightings"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+  
+  # Heatmap of sightings
+  output$heatmap_map <- renderLeaflet({
+  pal_heat <- colorNumeric("YlOrRd", domain = NULL)
+
+  leaflet(priority_sf()) %>%
+    addProviderTiles(providers$CartoDB.Positron) %>%
+    
+    # Add NDDB polygons first (drawn underneath the heatmap)
+    addPolygons(
+      data = nddb,
+      fillColor = "green",
+      fillOpacity = 0.3,
+      weight = 1,
+      color = "darkgreen",
+      group = "NDDB Areas"
+    ) %>%
+    
+    # Add heatmap layer
+    addHeatmap(
+      lng = ~longitude,
+      lat = ~latitude,
+      radius = 30,
+      blur = 1,
+      max = 0.15
+    ) %>%
+    
+    # Fit view to data
+    fitBounds(
+      lng1 = min(priority_sf()$longitude),
+      lat1 = min(priority_sf()$latitude),
+      lng2 = max(priority_sf()$longitude),
+      lat2 = max(priority_sf()$latitude)
+    ) %>%
+    
+    # Add heatmap legend
+    addLegend(
+      position = "bottomright",
+      pal = pal_heat,
+      values = c(0, 1),
+      title = "Sightings Density",
+      opacity = 0.7,
+      labFormat = function(type, cuts, p) {
+        paste0(round(cuts * 100), "%")
+      }
+    )
+})
+
+}
+
+shinyApp(ui, server)
